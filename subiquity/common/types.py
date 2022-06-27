f70177d906d688c341b6a0016332dce4a304ecf9
Dan Bungert <daniel.bungert@canonical.com>
2022-06-27 17:57:33 -0600

    start new guided api

--- subiquity/common/types.py
+++ subiquity/common/types.py
@@ -339,6 +339,55 @@ class GuidedResizeValues:
 
 
 @attr.s(auto_attribs=True)
+class GuidedStorageTargetReformat:
+    disk_id: str
+
+
+@attr.s(auto_attribs=True)
+class GuidedStorageTargetResize:
+    disk_id: str
+    partition_number: int
+    new_size: int
+    minimum: Optional[int]
+    recommended: Optional[int]
+    maximum: Optional[int]
+
+    @staticmethod
+    def from_recommendations(part, resize_vals):
+        return GuidedStorageTargetResize(
+                disk_id=part.device.id,
+                partition_number=part.number,
+                new_size=resize_vals.recommended,
+                minimum=resize_vals.minimum,
+                recommended=resize_vals.recommended,
+                maximum=resize_vals.maximum,
+                )
+
+
+@attr.s(auto_attribs=True)
+class GuidedStorageTargetUseGap:
+    disk_id: str
+    gap: Gap
+
+
+@attr.s(auto_attribs=True)
+class GuidedChoiceV2:
+    target: Union[GuidedStorageTargetReformat,
+                  GuidedStorageTargetResize,
+                  GuidedStorageTargetUseGap]
+    use_lvm: bool = False
+    password: Optional[str] = attr.ib(default=None, repr=False)
+
+
+@attr.s(auto_attribs=True)
+class GuidedStorageResponseV2:
+    configured: Optional[GuidedChoiceV2] = None
+    possible: List[Union[GuidedStorageTargetReformat,
+                         GuidedStorageTargetResize,
+                         GuidedStorageTargetUseGap]] = attr.Factory(list)
+
+
+@attr.s(auto_attribs=True)
 class AddPartitionV2:
     disk_id: str
     partition: Partition
