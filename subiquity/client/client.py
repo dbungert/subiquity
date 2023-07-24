e7a177c079b1bb8ac695b2c31385303ae7447ff1
Dan Bungert <daniel.bungert@canonical.com>
2023-07-24 11:37:56 -0600

    format

--- subiquity/client/client.py
+++ subiquity/client/client.py
@@ -54,8 +54,7 @@ class Abort(Exception):
         self.error_report_ref = error_report_ref
 
 
-DEBUG_SHELL_INTRO = _(
-    """\
+DEBUG_SHELL_INTRO = _("""\
 Installer shell session activated.
 
 This shell session is running inside the installer environment.  You
@@ -64,8 +63,7 @@ example by typing Control-D or 'exit'.
 
 Be aware that this is an ephemeral environment.  Changes to this
 environment will not survive a reboot. If the install has started, the
-installed system will be mounted at /target."""
-)
+installed system will be mounted at /target.""")
 
 
 class SubiquityClient(TuiApplication):
@@ -215,7 +213,7 @@ class SubiquityClient(TuiApplication):
         no = _("no")
         answer = no
         print(_("Confirmation is required to continue."))
-        print(_("Add 'autoinstall' to your" "kernel command line to avoid this"))
+        print(_("Add 'autoinstall' to your kernel command line to avoid this"))
         print()
         prompt = "\n\n{} ({}|{})".format(_("Continue with autoinstall?"), yes, no)
         while answer != yes:
